1727a752920d5fd4cf2dc0e34a5d916daf2dbecc
Jeff Pihach <jeff.pihach@canonical.com>
2018-06-07 12:31:04 -0600

    Add bakery library to pkg index.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-// Copyright (C) 2018 Canonical Ltd.
+/* Copyright (C) 2018 Canonical Ltd. */
+
 'use strict';
 
+const bakery = require('./lib/bakery');
 const urls = require('./lib/urls');
 
+
 module.exports = {
+  bakery,
   urls
 };
